f327de28246d5697d809a194af6a8db39fc4702f
Jorge Cuerdo Álvarez <jocual@gmail.com>
2018-05-21 23:24:08 +0200

    Add deploy sh

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+echo 'Deploying'
+
+echo 'Deleting current executable'
+rm main
+
+echo 'Downloading new version'
+wget 'https://github.com/jcuerdo/mymarket-app-go/releases/download/0.0.1/main'
+
+echo 'Setting privileges'
+chmod 755 main
+
+echo 'Killing current'
+kill -9 $(lsof -t -i:8080)
+
+echo 'Start service'
+nohup ./main < /dev/null > std.out 2> std.err &
